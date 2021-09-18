c4cee5996caa44952fe9741fc05f02fb46add188
esonboyev03@bk.ru <esonboyev03@gmail.com>
2021-09-18 12:26:12 +0000

    FIRTS NAME

---
+++ Dev.c
@@ -0,0 +1,14 @@
+#include <stdio.h> // standar  input:kirish ,outtput:chiqrish chqirish
+
+int main (){
+
+printf("Hello world");
+
+return 0;
+};
+
+
+// Compilyator bu bironbir dasturlash tiliga o'girib beradi  masalan || 010101
+// int funksiya turi.
+//printf ekranga chiqaruvchi funksya.
+
